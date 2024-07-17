983e78af810b64ea8869f1e5950e7bf3aefc3be1
elaya <ea.castillorayo.000@gmail.com>
2024-07-17 13:53:35 -0400

    hola

--- Juego-Cachipun-master/script.js
+++ Juego-Cachipun-master/script.js
@@ -1,11 +1,14 @@
 let empates = 0;
+let ptsBot =0;
+let ptsUsuario =0;  
+
 
 function mostrarNombre(){
     let nombre = document.querySelector('#nombreUsuario').value;
     document.querySelector('#jugador1Nombre').textContent=nombre;
 }
 
-function JugadorUsuario(jugada){
+function JugadaUsuario(jugada){
     if (jugada == 1){
         document.querySelector('#Jusuario').src ='img/piedra.png';
     }
@@ -16,30 +19,47 @@ function JugadorUsuario(jugada){
         document.querySelector('#Jusuario').src ='img/tijera.png';
     }
 
-   let bot = jugadabot()
-   Puntaje(jugada,bot);
+   let bot = Jugadabot();
+   PuntajeJuego(jugada,bot);
 }
 
 function Jugadabot(){
     let azar = Math.floor(Math.random()*3)+1
-    //let bot = document.getElementById('#Jbot');
     if(azar == 1){
-        document.querySelector('#Jbot').src = 'img/piedra.png'
+        document.querySelector('#Jbot').src = 'img/piedra.png';
     }
     if (azar == 2){
-        document.querySelector('#Jbot').src = 'img/papel.png'
+        document.querySelector('#Jbot').src = 'img/papel.png';
     }
     if (azar == 3){
-        document.querySelector('#Jbot').src = 'img/tijera.png'
+        document.querySelector('#Jbot').src = 'img/tijera.png';
     }
-    return azar;
+    return azar
 
 }
 
-function Puntaje(u,b){
+function PuntajeJuego(u,b){
     if((u==1 && b==1)||(u==2 && b==2)||(u==3 && b==3)){
         empates++;
         document.querySelector('#ptsEmpates').textContent= empates;
     }
+    if((u==2 && b==1)||(u==1 && b==3)||(u==3 && b==2)){
+        ptsUsuario++;
+        document.querySelector('#ptsJugador1').textContent= ptsUsuario;
+    }
+    if((u==1 && b==2)||(u==2 && b==3)||(u==3 && b==1)){
+        ptsBot++;
+        document.querySelector('#ptsJugador2').textContent= ptsBot;
+    }
+    
+}
+
+function Reiniciar(){
+    empates = 0;
+    ptsUsuario = 0;
+    ptsBot = 0;
+    document.querySelector('#ptsEmpates').textContent= empates;
+    document.querySelector('#ptsJugador1').textContent= ptsUsuario;
+    document.querySelector('#ptsJugador2').textContent= ptsBot;
 }
 
